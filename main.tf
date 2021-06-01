830b63e996c367359ca2a609b761a62affcc7ec2
jordanrasch <jordan.rasch@hpe.com>
2021-06-01 16:58:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,17 @@ provider "aws" {
 resource "aws_instance" "new-workspace" {
   instance_type =  "t2.micro"
   ami = "ami-0565af6e282977273"
+
+  tags {
+    Name = "first web server"
+  }
+}
+
+resource "aws_instance" "server_cluster" {
+  instance_type =  "t2.micro"
+  ami = "ami-0565af6e282977273"
+  count = 5
+  tags {
+    Name = "cluster server ${aws_instance.server_cluster.count[index]}"
+  }
 }
